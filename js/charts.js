c91c8e40593a5ff3ace48bb3db584eb87935f81c
Rob Spectre <rob.spectre@gmail.com>
2015-02-11 22:02:12 -0500

    1) Switched to C3 for regular charts. 2) Removed amCharts.

--- js/charts.js
+++ js/charts.js
@@ -1,62 +1,65 @@
 function advanceChart(slide) {
     if (slide.className.indexOf("chart-pie") > -1) {
-        var chart = AmCharts.makeChart("pie",{
-            "type"      : "pie",
-            "titleField"    : "category",
-            "valueField"    : "column-1",
-            "startEffect"   : "easeOutSine",
-            "startDuration" : 1,
-            "dataProvider"  : [
-                {
-                    "category": "category 1",
-                    "column-1": 8
-                },
-                {
-                    "category": "category 2",
-                    "column-1": 6
-                },
-                {
-                    "category": "category 3",
-                    "column-1": 2
-                }
-            ]
+        var chart = c3.generate({
+            bindto: '#pie',
+            data: {
+                columns: [
+                    ["versicolor", 1.4, 1.5, 1.5, 1.3, 1.5, 1.3, 1.6, 1.0, 1.3, 1.4, 1.0, 1.5, 1.0, 1.4, 1.3, 1.4, 1.5, 1.0, 1.5, 1.1, 1.8, 1.3, 1.5, 1.2, 1.3, 1.4, 1.4, 1.7, 1.5, 1.0, 1.1, 1.0, 1.2, 1.6, 1.5, 1.6, 1.5, 1.3, 1.3, 1.3, 1.2, 1.4, 1.2, 1.0, 1.3, 1.2, 1.3, 1.3, 1.1, 1.3],
+                    ["virginica", 2.5, 1.9, 2.1, 1.8, 2.2, 2.1, 1.7, 1.8, 1.8, 2.5, 2.0, 1.9, 2.1, 2.0, 2.4, 2.3, 1.8, 2.2, 2.3, 1.5, 2.3, 2.0, 2.0, 1.8, 2.1, 1.8, 1.8, 1.8, 2.1, 1.6, 1.9, 2.0, 2.2, 1.5, 1.4, 2.3, 2.4, 1.8, 1.8, 2.1, 2.4, 2.3, 1.9, 2.3, 2.5, 2.3, 1.9, 2.0, 2.3, 1.8],
+                    ["setosa", 30]
+                ],
+                type: 'pie'
+            }
         });
     } else if (slide.className.indexOf("chart-line") > -1) {
-        var chart = AmCharts.makeChart("line", {
-            "dataProvider"  : generateChartData(), 
-            "animationPlayed" : false,
-            "type": 'serial',
-            "graphs": [
-                {
-                    
-                    "valueField"    : "visits",
+        var chart = c3.generate({
+            bindto: '#line',
+            data: {
+                x: 'x',
+                columns: generateChartData(),
+            },
+            axis: {
+                x: {
+                    type: 'timeseries',
+                    tick: {
+                        format: '%Y-%m-%d',
+                    }
                 }
-            ]
+            }
         });
     } else if (slide.className.indexOf("chart-bar") > -1) {
-        var chart = AmCharts.makeChart("bar", {
-            "dataProvider"  : generateChartData(), 
-            "animationPlayed" : true,
-            "type": 'serial',
-            "graphs": [
-                {
-                    "type": "column",
-                    "valueField"    : "visits",
-                    "fillAlphas": 1 
+        var chart = c3.generate({
+            bindto: '#bar',
+            data: {
+                x: 'x',
+                columns: generateChartData(),
+                type: 'bar'
+            },
+            axis: {
+                x: {
+                    type: 'timeseries',
+                    tick: {
+                        format: '%Y-%m-%d',
+                    }
                 }
-            ]
+            }
         });
     } else if (slide.className.indexOf("chart-area") > -1) {
-        var chart = AmCharts.makeChart("area", {
-            "dataProvider"  : generateChartData(), 
-            "animationPlayed" : true,
-            "type": 'serial',
-            "graphs": [
-                {
-                    "valueField"    : "visits",
-                    "fillAlphas": 1 
+        var chart = c3.generate({
+            bindto: '#area',
+            data: {
+                x: 'x',
+                columns: generateChartData(),
+                type: 'area'
+            },
+            axis: {
+                x: {
+                    type: 'timeseries',
+                    tick: {
+                        format: '%Y-%m-%d',
+                    }
                 }
-            ]
+            }
         });
     }
 
@@ -78,8 +81,14 @@ function generateChartData() {
 
         chartData.push({
             date: newDate,
-            visits: visits
-        });
+            visits: visits});
+        };
+
+    var x = ['x'];
+    var visits = ['Visits'];
+    for (i=0; i < 100; i++) {
+        x.push(chartData[i].date);
+        visits.push(chartData[i].visits);
     }
-    return chartData;
+    return [x, visits];
 }
